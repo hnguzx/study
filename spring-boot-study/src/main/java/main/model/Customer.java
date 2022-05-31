026a9cd5cf70d68a5a0f19f4654141fb23ef86ad
hnguzx <2544697696@qq.com>
2022-05-31 15:32:19 +0800

    .gitignore is now working

--- spring-boot-study/src/main/java/main/model/Customer.java
+++ spring-boot-study/src/main/java/main/model/Customer.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * @author 志雄
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Customer {
+public class Customer implements Serializable {
     private Integer id;
     private String username;
     private String password;
--- spring-boot-study/src/test/java/main/java/InterviewTest.java
+++ spring-boot-study/src/test/java/main/java/InterviewTest.java
@@ -6,11 +6,16 @@ import main.util.JsonUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.Math.*;
 
@@ -55,7 +60,7 @@ public class InterviewTest {
         byte b2 = 127;
 
 //        b2 = b1+b2;
-        b2+=b1;
+        b2 += b1;
 
         short var1 = 1;
 //        var1 = var1 + 1;
@@ -64,7 +69,7 @@ public class InterviewTest {
         Field[] fields = aClass.getFields();
         Method[] methods = aClass.getMethods();
         Field declaredField = aClass.getDeclaredField("");
-        Method declaredMethod = aClass.getDeclaredMethod("",null);
+        Method declaredMethod = aClass.getDeclaredMethod("", null);
         Constructor<?> constructor = aClass.getConstructor(null);
         Class<?>[] parameterTypes = constructor.getParameterTypes();
         CustomerServiceImpl o = (CustomerServiceImpl) constructor.newInstance(null);
@@ -74,6 +79,104 @@ public class InterviewTest {
         ArrayList arrayList = new ArrayList();
         arrayList.ensureCapacity(20);
 
+
+    }
+
+    private final StringBuilder finalString = new StringBuilder("guzx");
+
+    class Inner extends InterviewTest {
+        public Inner() {
+            super();
+        }
+    }
+
+    @Test
+    public void testFinal() {
+        log.info(finalString.toString());
+        StringBuilder s = finalString;
+        s = s.append("@qq.com");
+        log.info(finalString.toString());
+        log.info(s.toString());
+
+        String a = "1asd";
+        String b = "1asd";
+        b.equals(a);
+        log.info("a==b?{}", a == b);
+    }
+
+    @Test
+    public void testFor() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 1) {
+                    continue;
+                }
+                log.info("i:{},j:{}", i, j);
+            }
+        }
+    }
+
+    @Test
+    public void testValue() {
+        StringBuilder a = new StringBuilder("guzx");
+        log.info("a:{}", a.toString());
+        StringBuilder b = add(a);
+        log.info("a:{},b:{}", a, b);
+    }
+
+    public StringBuilder add(StringBuilder a) {
+        StringBuilder b = new StringBuilder();
+        a = new StringBuilder();
+        b = a.append("111");
+        b = null;
+        return b;
+    }
+
+    @Test
+    public void testIO() {
+        /*FileReader fileReader = new FileReader();
+        FileWriter fileWriter = new FileWriter();
+
+        FileInputStream fileInputStream = new FileInputStream();
+        FileOutputStream fileOutputStream = new FileOutputStream();
+
+        Class<FileInputStream> fileInputStreamClass = FileInputStream.class;*/
+    }
+
+    @Test
+    public void testChar() throws ClassNotFoundException {
+        char a = 'a';
+        int i = a + 12;
+        log.info("i:{}", i);
+
+        char[] chars = {'a', 'b'};
+        int length = chars.length;
+        String s = "123";
+        int length1 = s.length();
+
+        List<char[]> chars1 = Arrays.asList(chars);
+        Object[] objects = chars1.toArray();
+
+        List list = new ArrayList();
+        ListIterator listIterator = list.listIterator();
+        list.add(1);
+
+        list.add(2.3);
+        list.add(false);
+        log.info(list.toString());
+        for (Object item : list) {
+            log.info("取出顺序：{}", item.toString());
+        }
+
+        Object[] objects1 = {1, 2.3f, false};
+        for (int j = 0; j < objects1.length; j++) {
+            log.info("数组取出顺序：{}", objects1[j]);
+        }
+
+//        ClassLoader loader = ClassLoader.getSystemClassLoader();
+//        loader.loadClass("");
+
+        Map map = new HashMap();
 
     }
 }
