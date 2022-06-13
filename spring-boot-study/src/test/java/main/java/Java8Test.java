ad940a95a9d387969ffc8581b37630c26505ddaf
hnguzx <2544697696@qq.com>
2022-06-13 19:39:29 +0800

    添加java8 date与LocalDate相关测试

--- spring-boot-study/src/test/java/main/java/Java8Test.java
+++ spring-boot-study/src/test/java/main/java/Java8Test.java
@@ -1,10 +1,17 @@
 package main.java;
 
 import lombok.extern.slf4j.Slf4j;
+import main.model.Customer;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class Java8Test {
@@ -77,8 +84,9 @@ public class Java8Test {
 
     @Test
     void testSupplier() {
-        Supplier<String> supplier = String::new;
-        String s = supplier.get();
+        Supplier<Customer> supplier = Customer::new;
+        Customer s = supplier.get();
+        System.out.println(s.getUsername());
     }
 
     @Test
@@ -101,8 +109,7 @@ public class Java8Test {
 
     @Test
     void testConsumer() {
-        Consumer<String> consumer = item ->
-                log.info("consumer:{}", item);
+        Consumer<String> consumer = item -> log.info("consumer:{}", item);
         consumer.accept("guzhixiong");
     }
 
@@ -117,8 +124,80 @@ public class Java8Test {
     @Test
     void testStream() {
         List<String> list = Arrays.asList("gzx", "lh", "gzx");
-        list.parallelStream().filter(item -> item.length() > 2).map(item -> item.toUpperCase()).distinct().forEach(System.out::println);
+        list.stream().filter(item -> item.length() > 2).map(item -> item.toUpperCase()).distinct().forEach(System.out::println);
         list.parallelStream().filter(item -> item.length() > 2).map(String::toUpperCase).distinct().forEach(System.out::println);
+
+        //同时获取最大 最小 平均值等信息
+        List<Integer> list1 = Arrays.asList(1, 3, 5, 7, 9, 11);
+        IntSummaryStatistics statistics = list1.stream().filter(integer -> integer > 2).mapToInt(i -> i * 2).skip(2).limit(2).summaryStatistics();
+        System.out.println(statistics.getMax());//18
+        System.out.println(statistics.getMin());//14
+        System.out.println(statistics.getAverage());//16
+    }
+
+    @Test
+    void testDate() {
+//        Zoned 时区
+//        DateTimeFormatter 格式化
+//        Period 日期间隔
+//        Duration 时间间隔
+        // date LocalDate
+        LocalDate now = LocalDate.now();
+        log.info("now:{}", now);
+        int year = now.getYear();
+        Month month = now.getMonth();
+        int monthValue = month.getValue();
+        int dayOfMonth = now.getDayOfMonth();
+        DayOfWeek dayOfWeek = now.getDayOfWeek();
+        int weekValue = dayOfWeek.getValue();
+        log.info("year:{},month:{},dayOfMonth:{},dayOfWeek:{}", year, monthValue, dayOfMonth, weekValue);
+
+        LocalDate localDate = now.plusDays(6L);
+        log.info("格式化date：{}", localDate.format(DateTimeFormatter.ISO_DATE));
+
+        // time LocalTime LocalDateTime
+        LocalTime time = LocalTime.now();
+        log.info("time:{}", time.toString());
+        int hour = time.getHour();
+        int minute = time.getMinute();
+        int second = time.getSecond();
+        log.info("hour:{},minute:{},second:{}", hour, minute, second);
+        log.info("格式化time：{}", time.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+
+        LocalDateTime dateTime = LocalDateTime.now().plusSeconds(10);
+        log.info("dateTime:{}", dateTime);
+        log.info("格式化dateTime：{}", dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        log.info("格式化dateTime：{}", dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss")));
+
+        LocalDate parse = LocalDate.parse("2021-06-12");
+        log.info("after：{}", parse.isAfter(now));
+        log.info("after：{}", now.isAfter(parse));
+        log.info("withYear:{}",now.withYear(2025));
+        log.info("withMonth:{}",now.withMonth(4));
+        log.info("withDayOfYear:{}",now.withDayOfYear(78));
+
+        Date date = new Date();
+        LocalDateTime dateTime1 = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        log.info("date to localDateTime:{}",dateTime1);
+        Calendar calendar = Calendar.getInstance();
+        LocalDateTime dateTime2 = LocalDateTime.ofInstant(calendar.toInstant(), ZoneId.systemDefault());
+        log.info("calendar to localDateTime:{}",dateTime2);
+
+        Date from = Date.from(dateTime1.atZone(ZoneId.systemDefault()).toInstant());
+        GregorianCalendar from1 = GregorianCalendar.from(ZonedDateTime.of(dateTime1, ZoneId.systemDefault()));
+        log.info("localDateTime to date:{}",from);
+        log.info("localDateTime to calendar:{}",from1.getTime());
+
+    }
+
+    @Test
+    void testBase64() {
+        Base64.Encoder encoder = Base64.getEncoder();
+        Base64.Decoder decoder = Base64.getDecoder();
+
+        byte[] encode = encoder.encode("guzhixiong".getBytes());
+        byte[] decode = decoder.decode(encode);
+        log.info("decode:{}", new String(decode, StandardCharsets.UTF_8));
     }
 
 }
